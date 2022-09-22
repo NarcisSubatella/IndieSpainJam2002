59c9f4c17fc6c723ab1c308b1c249ea504dcffad
Narcís Subatella <nsubatella@gmail.com>
2022-09-22 02:20:59 +0200

    UpdateMenu

--- Assets/UI/MenuButtonsFeedback.cs
+++ Assets/UI/MenuButtonsFeedback.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class MenuButtonsFeedback : MonoBehaviour
+public class MenuButtonsFeedback : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     MenuManager menu;
     bool soundPlayed = false;
@@ -12,31 +13,38 @@ public class MenuButtonsFeedback : MonoBehaviour
     void Awake()
     {
         menu = GameObject.FindGameObjectWithTag("MenuController").GetComponent<MenuManager>();
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    /*
+
     public void OnPointerEnter(PointerEventData pointerEventData)
     {
-        print("HOLA CABRON");
+        if (soundPlayed == false)
+        {
+
+            menu.HoverButton();
+            LeanTween.scale(transform.GetChild(0).gameObject, new Vector3(0.10f, 0.10f, 0.10f), 0.2f).setEaseInOutCubic();
+            soundPlayed = true;
+        }
     }
     public void OnPointerExit(PointerEventData pointerEventData)
     {
-        print("ADIOS CABRON");
+        LeanTween.scale(transform.GetChild(0).gameObject, new Vector3(0.65f, 0.65f, 0.65f), 0.2f).setEaseInOutCubic();
+        soundPlayed = false;
     }
-    
+    /*
     private void OnMouseOver()
     {
         print("HOLA CABRON");
         if (soundPlayed == false)
         {
-            
+
             menu.HoverButton();
             LeanTween.scale(this.gameObject, new Vector3(0.10f, 0.10f, 0.10f), 0.2f).setEaseInOutCubic();
             soundPlayed = true;
